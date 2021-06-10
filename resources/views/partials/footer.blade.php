5366e316c7123c4fa786acdd58e325756e718ef4
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-06-10 09:30:19 +0200

    Finished Homepage.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,110 @@
 <footer>
+
+    <div class="footer-bg">
+
+        <div class="container">
+
+            <div class="footer-nav">
+
+                <div class="footer-logo">
+                    <img src="{{ asset('images/dc-logo-bg.png') }}" alt="DC Logo">
+                </div>
+
+                <div class="flex">
+
+                    <div class="col">
+
+                        <ul>
+                            <li class="list-title">DC Comics</li>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
     
+                        <ul>
+                            <li class="list-title">Shop</li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+    
+                    </div>
+
+                    <div class="col">
+
+                        <ul>
+                            <li class="list-title">DC</li>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+    
+                    </div>
+
+                    <div class="col">
+
+                        <ul>
+                            <li class="list-title">Sites</li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+
+                    </div>
+
+                </div>
+
+                <div class="footer-copyright">
+                    <div>All Site Content TM and <i class="far fa-copyright"></i> 2020 Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</div>
+                    <div><a href="#">Cookie Settings</a></div>
+                </div>    
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+    <div class="footer-signup">
+
+        <div class="container">
+
+            <div class="flex">
+
+                <div>
+                    <a class="button-signup" href="#">Sign-up now!</a>
+                </div>
+
+                <div>
+                    <ul class="flex">
+                        <li>Follow Us</li>
+                        <li><img src="{{ asset('images/footer-facebook.png') }}" alt="Facebook"></li>
+                        <li><img src="{{ asset('images/footer-twitter.png') }}" alt="Twitter"></li>
+                        <li><img src="{{ asset('images/footer-youtube.png') }}" alt="Youtube"></li>
+                        <li><img src="{{ asset('images/footer-pinterest.png') }}" alt="Pinterest"></li>
+                        <li><img src="{{ asset('images/footer-periscope.png') }}" alt="Periscope"></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
 </footer>
